e5dc465108dc42c9c020e5a117d853c098562871
anhtrangggg <106827031+anhtrangggg@users.noreply.github.com>
2022-06-03 23:20:00 +0700

    Update config.js

--- config.js
+++ config.js
@@ -7,11 +7,11 @@ const CONFIG = {
     desc: ' Nếu cậu không trả lời là cậu đồng ý làm bồ tớ gòi đó >< ',
     btnYes: 'Tớ cũng thích cậu <3',
     btnNo: 'Không nha :3',
-    question: 'Babe babe chỉ cần nhìn thấy cậu cười chút thoii, là lòng tớ đã trót nhớ nhung roiii :3 ',
+    question: 'Babe babe chỉ cần nhìn thấy cậu cười chút thoii, là lòng tớ trót nhớ nhung roiii :3 ',
     btnReply: 'Gửi cho bạn <3',
-    reply: ' iu cậu <33333333',
-    mess: 'Tớ biết mà 🥰. Iu cậu ahihi 😘',
-    messDesc: 'Hôm nào rảnh mình đi chơi nha.',
+    reply: ' iu cậu quá nè <33333333',
+    mess: 'Tớ biết mà 🥰. iu cậu ahihi 😘',
+    messDesc: 'Hôm nào rảnh chúng mình đi chơi nha.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/profile.php?id=100066573210065' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
